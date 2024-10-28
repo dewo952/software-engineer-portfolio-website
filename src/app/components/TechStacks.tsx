ef6292bc7ae932157f51b86fb6c3567200eed18c
Diven Khatri <diven.khatri@rgs-tech.com>
2024-10-28 10:18:01 +0530

    update

--- src/app/components/TechStacks.tsx
+++ src/app/components/TechStacks.tsx
@@ -5,6 +5,7 @@ import techStacksData from "./techstack.json";
 import FadeUp from "@/animations/FadeUp";
 import { Poppins } from "next/font/google";
 import Image from "next/image";
+import GradientTxt from "./Reusables/GradientTxt";
 
 const font = Poppins({
   subsets: ["latin"],
@@ -29,8 +30,13 @@ export default function TechStacks() {
   return (
     <section className="mb-[108px]">
       <FadeUp tag="div" className="mb-16">
+        <GradientTxt
+          tagName="h6"
+          txt="TECHNOLOGIES I USE"
+          className="mb-4 text-center text-[22px] font-bold"
+        />
         <h2 className="mx-auto mb-10 max-w-[800px] text-center text-[36px] font-bold leading-[120%] tracking-[0.5px] lg:text-[54px]">
-          Crafting solutions through code, one product at a time.
+          Equipped with Skills for Every Challenge
         </h2>
       </FadeUp>
       <div className="relative overflow-hidden">
@@ -40,7 +46,10 @@ export default function TechStacks() {
           animate="animate"
         >
           {techStacksData.map((tech) => (
-            <div key={tech.technology} className="flex-shrink-0 flex items-center">
+            <div
+              key={tech.technology}
+              className="flex flex-shrink-0 items-center"
+            >
               <Image
                 src={tech.icon.url}
                 alt={tech.icon.alt}
@@ -50,7 +59,10 @@ export default function TechStacks() {
             </div>
           ))}
           {techStacksData.map((tech) => (
-            <div key={tech.technology} className="flex-shrink-0 flex items-center">
+            <div
+              key={tech.technology}
+              className="flex flex-shrink-0 items-center"
+            >
               <Image
                 src={tech.icon.url}
                 alt={tech.icon.alt}
